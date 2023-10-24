0f3aec34be5c3dbdeb7a4085f60e4872ef0ef2b3
Marcin Grawiński <116593541+Grawikos@users.noreply.github.com>
2023-10-24 16:40:44 +0200

    Update TestComplex.cpp
    Final tweaks

--- TestComplex.cpp
+++ TestComplex.cpp
@@ -3,127 +3,140 @@
 
 using namespace std;
 
-void checkBinaryOp(ComplexNum complexA, ComplexNum complexB, double dA, double dB){
+void checkBinaryOp()
+{
+	ComplexNum complexA(2, 2), complexB(-3, -3);
+	double dA = 2;
+
 	cout << "Check binary operations" << endl;
-	std::cout << "a = " << complexA << endl;
-	std::cout << "b = " << complexB << endl;
-    
-    std::cout << "a + b = " << complexA + complexB << endl;
-    std::cout << "a - b = " << complexA - complexB << endl;
-    std::cout << "a * b = " << complexA * complexB << endl;
-    std::cout << "a / b = " << complexA / complexB << endl;
-    
-    std::cout << "a + dA = " << complexA + dA << endl; 
-    std::cout << "a - dA = " << complexA - dA << endl;
-    std::cout << "a * dA = " << complexA * dA << endl;
-    std::cout << "a / dA = " << complexA / dA << endl;
-    
-    std::cout << "dA + a = " << dA + complexA << endl;
-    std::cout << "dA - a = " << dA - complexA << endl;
-    std::cout << "dA * a = " << dA * complexA << endl;
-    std::cout << "dA / a = " << dA / complexA << endl;
-    
+	cout << "a = " << complexA << endl;
+	cout << "b = " << complexB << endl;
+	cout << "dA = " << dA << endl;
+
+	cout << "a + b = " << complexA + complexB << endl;
+	cout << "a - b = " << complexA - complexB << endl;
+	cout << "a * b = " << complexA * complexB << endl;
+	cout << "a / b = " << complexA / complexB << endl;
+
+	cout << "a + dA = " << complexA + dA << endl;
+	cout << "a - dA = " << complexA - dA << endl;
+	cout << "a * dA = " << complexA * dA << endl;
+	cout << "a / dA = " << complexA / dA << endl;
+
+	cout << "dA + a = " << dA + complexA << endl;
+	cout << "dA - a = " << dA - complexA << endl;
+	cout << "dA * a = " << dA * complexA << endl;
+	cout << "dA / a = " << dA / complexA << endl;
 }
 
+void testEquality()
+{
+	ComplexNum a(2, 3);
+	ComplexNum b(2, 3);
+	ComplexNum c(3, 4);
 
-void testEquality() {
-    ComplexNum a(2, 3);
-    ComplexNum b(2, 3);
-    ComplexNum c(3, 4);
-    
-    std::cout << "Equality Test" << std::endl;
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
-    std::cout << "c = " << c << std::endl;
-    
-    std::cout << "a == b: " << (a == b) << std::endl;
-    std::cout << "a != b: " << (a != b) << std::endl;
-    std::cout << "a == c: " << (a == c) << std::endl;
-    std::cout << "a != c: " << (a != c) << std::endl;
-    
-    ComplexNum d;
-    ComplexNum e(5, 0);
-    std::cout << "d = " << d << std::endl;
-    std::cout << "e = " << e << std::endl;
-    std::cout << "d == 0: " << (d == 0) << std::endl;
-    std::cout << "d != 0: " << (d != 0) << std::endl;
-    std::cout << "5 == e: " << (5 == e) << std::endl;
-    std::cout << "5 != e: " << (5 != e) << std::endl;
+	cout << "Equality Test" << endl;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+
+	cout << "a == b: " << (a == b) << endl;
+	cout << "a != b: " << (a != b) << endl;
+	cout << "a == c: " << (a == c) << endl;
+	cout << "a != c: " << (a != c) << endl;
+
+	ComplexNum d;
+	ComplexNum e(5, 0);
+	cout << "d = " << d << endl;
+	cout << "e = " << e << endl;
+	cout << "d == 0: " << (d == 0) << endl;
+	cout << "d != 0: " << (d != 0) << endl;
+	cout << "5 == e: " << (5 == e) << endl;
+	cout << "5 != e: " << (5 != e) << endl;
 }
 
-void testUnaryOp() {
-    ComplexNum a(2, 3);
-    ComplexNum b(1, 2);
-    double d = 4;
+void testArithAssingmentOp()
+{
+	ComplexNum a(2, 3);
+	ComplexNum b(1, 2);
+	double d = 4;
 
-    std::cout << "Check compound assignment operations" << std::endl;
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
-    std::cout << "d = " << d << std::endl;
+	cout << "Check compound assignment operations" << endl;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "d = " << d << endl;
 
-    a += b;
-    std::cout << "a += b: a = " << a << std::endl;
+	a += b;
+	cout << "a += b: a = " << a << endl;
 
-    a = ComplexNum(2, 3);
-    a -= b;
-    std::cout << "a -= b: a = " << a << std::endl;
+	a = ComplexNum(2, 3);
+	a -= b;
+	cout << "a -= b: a = " << a << endl;
 
-    a = ComplexNum(2, 3);
-    a *= b;
-    std::cout << "a *= b: a = " << a << std::endl;
+	a = ComplexNum(2, 3);
+	a *= b;
+	cout << "a *= b: a = " << a << endl;
 
-    a = ComplexNum(2, 3);
-    a /= b;
-    std::cout << "a /= b: a = " << a << std::endl;
+	a = ComplexNum(2, 3);
+	a /= b;
+	cout << "a /= b: a = " << a << endl;
 
-    a = ComplexNum(2, 3);
-    a += d;
-    std::cout << "a += d: a = " << a << std::endl;
+	a = ComplexNum(2, 3);
+	a += d;
+	cout << "a += d: a = " << a << endl;
 
-    a = ComplexNum(2, 3);
-    a -= d;
-    std::cout << "a -= d: a = " << a << std::endl;
+	a = ComplexNum(2, 3);
+	a -= d;
+	cout << "a -= d: a = " << a << endl;
 
-    a = ComplexNum(2, 3);
-    a *= d;
-    std::cout << "a *= d: a = " << a << std::endl;
+	a = ComplexNum(2, 3);
+	a *= d;
+	cout << "a *= d: a = " << a << endl;
 
-    a = ComplexNum(2, 3);
-    a /= d;
-    std::cout << "a /= d: a = " << a << std::endl;
+	a = ComplexNum(2, 3);
+	a /= d;
+	cout << "a /= d: a = " << a << endl;
 }
 
-void ambigousTests(){
-	ComplexNum a(1, 1), b(2, 2), c(3, 3);
+void ambigousTests()
+{
+	cout << "Ambigous Tests:" << endl;
+
+	ComplexNum a(1, 1), b(2, 2), c(3, 3), d(-1, -2);
+	cout << "a = " << a << "\nb = " << b << "\nc = " << c << endl;
 	a += b += c;
-	cout << "a += b += c\na = " << a << std::endl;
+	cout << "a += b += c\na = " << a << endl;
 	a = ComplexNum(1, 1);
 	b = ComplexNum(2, 2);
 	c = ComplexNum(3, 3);
 	(a += b) += c;
-	cout << "(a += b) += c\na = " << a << std::endl;
+	cout << "(a += b) += c\na = " << a << endl;
 	a = ComplexNum(1, 1);
 	b = ComplexNum(2, 2);
 	c = ComplexNum(3, 3);
 	a *= b *= c;
-	cout << "a *= b *= c\na = " << a << std::endl;
+	cout << "a *= b *= c\na = " << a << endl;
 	a = ComplexNum(1, 1);
 	b = ComplexNum(2, 2);
 	c = ComplexNum(3, 3);
 	(a *= b) *= c;
-	cout << "(a *= b) *= c\na = " << a << std::endl;
-	
-	
+	cout << "(a *= b) *= c\na = " << a << endl;
+	a = ComplexNum(1, 1);
+	cout << "-a: " << -a << endl;
+
+	cout << "Phase of a:\n" << a.phase() * 180 / M_PI << endl;
+	cout << "Magnitude of a:\n" << a.magnitude() << endl;
+
+	cout << "Phase of " << d << ":\n" << d.phase() * 180 / M_PI << endl;
+	cout << "Magnitude of " << d << ":\n" << d.magnitude() << endl;
 }
 
 int main()
 {
-	ComplexNum a(2, 2), b(-2, -2);
-	
-	checkBinaryOp(a, b, 2, 5);
-	testUnaryOp();
+	checkBinaryOp();
+	testArithAssingmentOp();
 	testEquality();
 	ambigousTests();
-	
+
 	return 0;
 }
