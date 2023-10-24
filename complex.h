13c5f3a755c0a85b99da319cc280a88a490862e0
Marcin Grawiński <116593541+Grawikos@users.noreply.github.com>
2023-10-24 16:42:24 +0200

    Update complex.h
    Final tweaks

--- complex.h
+++ complex.h
@@ -1,47 +1,50 @@
-#include <iostream>
+#include <cmath>
 #include <fstream>
+#include <iostream>
 #pragma once
 
 class ComplexNum {
-public:
-    
-    double real;
-    double imaginary;
-	
+  public:
+	double real;
+	double imaginary;
+
 	ComplexNum(double real = 0, double imaginary = 0);
 	ComplexNum(const ComplexNum& c);
 	ComplexNum& operator=(const ComplexNum& c);
 	friend std::ostream& operator<<(std::ostream& Out, const ComplexNum& c);
 	ComplexNum operator+(const ComplexNum& c) const;
-	ComplexNum operator+(const double &c) const;
-	
+	ComplexNum operator+(const double& c) const;
+
 	ComplexNum operator-(const ComplexNum& c) const;
-	ComplexNum operator-(const double &c) const;
-	
+	ComplexNum operator-(const double& c) const;
+	ComplexNum operator-();
+
 	ComplexNum operator*(const ComplexNum& c) const;
-	ComplexNum operator*(const double &c) const;
-	
+	ComplexNum operator*(const double& c) const;
+
 	ComplexNum operator/(const ComplexNum& c) const;
-	ComplexNum operator/(const double &c) const;
-	
+	ComplexNum operator/(const double& c) const;
+
 	ComplexNum& operator+=(const ComplexNum& c);
-    ComplexNum& operator+=(const double& d);
-    
-    ComplexNum& operator-=(const ComplexNum& c);
-    ComplexNum& operator-=(const double& d);
-    
-    ComplexNum& operator*=(const ComplexNum& c);
-    ComplexNum& operator*=(const double& d);
-    
-    ComplexNum& operator/=(const ComplexNum& c);
-    ComplexNum& operator/=(const double& d);
-    
-    bool operator==(const ComplexNum& c) const;
-    bool operator==(const double& d) const;
-    
-    bool operator!=(const ComplexNum& c) const;
-    bool operator!=(const double d) const;
-	
+	ComplexNum& operator+=(const double& d);
+
+	ComplexNum& operator-=(const ComplexNum& c);
+	ComplexNum& operator-=(const double& d);
+
+	ComplexNum& operator*=(const ComplexNum& c);
+	ComplexNum& operator*=(const double& d);
+
+	ComplexNum& operator/=(const ComplexNum& c);
+	ComplexNum& operator/=(const double& d);
+
+	bool operator==(const ComplexNum& c) const;
+	bool operator==(const double& d) const;
+
+	bool operator!=(const ComplexNum& c) const;
+	bool operator!=(const double& d) const;
+
+	double phase();
+	double magnitude() const;
 };
 
 ComplexNum operator+(const double d, const ComplexNum& c);
